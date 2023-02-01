9c9019ffd7da2a87f96e4c2e96cdac23bc91e022
ahammednabi245 <kaziahammed245nabi@gmail.com>
2023-02-02 00:53:44 +0600

    tempu

---
+++ tempu.js
@@ -0,0 +1,18 @@
+let first = 5;
+let second = 7;
+
+console.log(first, second);
+// first = second;
+// second = first;
+
+const tempu = first;
+first = second;
+second = tempu;
+console.log( first, second);
+
+
+
+// Destructuring--------------------------
+
+[ first, second ] = [ second, first];
+console.log(first, second);
