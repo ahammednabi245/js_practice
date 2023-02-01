4b23610444f43d7ee26f63492ff05c7a8b8ca229
ahammednabi245 <kaziahammed245nabi@gmail.com>
2023-02-01 21:26:29 +0600

    loop Reverse

---
+++ factorial-while-reverse.js
@@ -0,0 +1,13 @@
+function factorial(number){
+    let i = 7;
+    let result = 1;
+    while( i >= 1){
+        result = result * i ;
+        i--;
+
+    }
+    return result;
+}
+
+const output = factorial(7);
+console.log(output)
