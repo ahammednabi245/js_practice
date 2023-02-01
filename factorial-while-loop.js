fff9e3ad48800a271f9f7916bc8ec809a7fd4ce4
ahammednabi245 <kaziahammed245nabi@gmail.com>
2023-02-01 21:23:17 +0600

    whileloop

---
+++ factorial-while-loop.js
@@ -0,0 +1,12 @@
+function factorial(number){
+    let num = 1;
+    let result = 1;
+    while(num <= 7){
+        result= result + num;
+        num++
+        console.log(number);
+    }
+    return result;
+}
+
+factorial(1, 2, 3, 4, 5, 6, 7);
