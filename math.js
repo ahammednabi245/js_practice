065e2c1933d7f737007637d11b2d930b406d73bc
ahammednabi245 <kaziahammed245nabi@gmail.com>
2023-02-02 00:27:45 +0600

    math

---
+++ math.js
@@ -0,0 +1,37 @@
+const result = Math.pow(3, 8);
+console.log(result);
+
+const num1 = 25;
+const num2 = 45;
+
+const gap = Math.abs(num1 - num2);
+console.log(gap)
+
+if(gap < 5){
+    console.log('You guys can be friend');
+}
+else{
+    console.log('you guys stay a part');
+}
+
+
+
+// ---------------------------------------
+
+
+
+
+const number = 2.594598;
+const fullNumber = Math.round(number);
+
+// const fullNumber = Math.ceil(2.00001);
+// const fullNumber = Math.floor(456.259);
+
+console.log(fullNumber);
+
+
+
+// Random-----
+
+
+console.log(Math.random());
