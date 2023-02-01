8f65014aaf962975f088918e6b380355375df931
ahammednabi245 <kaziahammed245nabi@gmail.com>
2023-02-01 22:24:24 +0600

    includes

---
+++ search.js
@@ -0,0 +1,8 @@
+const lyrics = "Tumi bondhu kala pakhi ami jeno ki "
+
+const searchString =' pakhi';
+// const doseExist =  lyrics.includes('ami jeno');
+const doseExist =  lyrics.includes(searchString);
+
+console.log(doseExist);
+
