e4aef36ba7527a4a75601ba9eeb7e269501f8d73
ahammednabi245 <kaziahammed245nabi@gmail.com>
2023-02-01 21:57:35 +0600

    case

---
+++ case.js
@@ -0,0 +1,10 @@
+const userName = 'blackpink';
+const userInput = 'blackpinK';
+console.log(userName.toLowerCase());
+if( userName.toLowerCase() === userInput.toLowerCase()){
+    console.log('valid user')
+}
+
+else{
+    console.log('invalid User')
+}
