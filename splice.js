a2a8f1329ec2718f45e93b751ea562b50957e8f7
ahammednabi245 <kaziahammed245nabi@gmail.com>
2023-02-02 21:38:35 +0600

    splice

---
+++ splice.js
@@ -0,0 +1,12 @@
+const friends = [ 12, 45, 32, 22, 44, 62, 29, 69, 87]
+const partial = friends.splice(2, 5, 121, 131, 141, 151, 161,)
+
+console.log(partial);
+console.log(friends)
+
+
+const fruit = [ 'mango', 'Banana', 'jack-fruits', 'jambura', 'apple', 'orange' ]
+const fruits = fruit.splice(1, 3)
+
+console.log(fruits);
+console.log(fruit)
