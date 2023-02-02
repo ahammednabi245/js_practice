aa107a95ab271bf55cd74b63d23e767081964f56
ahammednabi245 <kaziahammed245nabi@gmail.com>
2023-02-02 11:10:30 +0600

    max

---
+++ max.js
@@ -0,0 +1,14 @@
+const jim = 84;
+const dele = 99;
+const chinku = 77;
+
+if( jim > dele && jim > chinku){
+    console.log('jim will get the cake');
+}
+
+else if( dele > jim && dele > chinku){
+    console.log('dele will get the cake ');
+}
+else{
+    console.log('chinku will get the cake')
+}
