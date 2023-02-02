802659c4568ef5d1e09a29a8d3ce09aed6df377f
ahammednabi245 <kaziahammed245nabi@gmail.com>
2023-02-02 21:17:57 +0600

    slice

---
+++ slice.js
@@ -0,0 +1,10 @@
+const friends = [ 12, 45, 32, 22, 44, 62, 29, 69, 87]
+const partial = friends.slice(2, 5)
+
+console.log(partial);
+
+
+const fruit = [ 'mango', 'Banana', 'jack-fruits', 'jambura', 'apple', 'orange' ]
+const fruits = fruit.slice(1, 5)
+
+console.log(fruits);
