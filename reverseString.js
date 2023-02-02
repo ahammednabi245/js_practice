6fa688b87c142020ff0e9758cabbaadbbdd2fdcc
ahammednabi245 <kaziahammed245nabi@gmail.com>
2023-02-02 12:26:06 +0600

    reverseString

---
+++ reverseString.js
@@ -0,0 +1,15 @@
+function reverseString(text){
+    // for(let i = 0; i < text.length; i++){}
+    let reverse = '';
+    for(let i = text.length-1; i >= 0; i--){
+        const element = text[i];
+
+        reverse = reverse + element;
+        console.log(element, reverse);
+    }
+    return reverse;
+}
+
+const myString = 'I am a good boy';
+const reverse =reverseString(myString);
+console.log('Reverse Output:',  reverse);
