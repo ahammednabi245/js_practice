f7040c2c44dcc072df290e5f0422ceedb0a480ef
ahammednabi245 <kaziahammed245nabi@gmail.com>
2023-02-04 19:43:35 +0600

    nayoks

--- forLoop.js
+++ forLoop.js
@@ -1,3 +1,16 @@
 for(let i = 0; i < 100; i++){
     console.log('I Love You Allah')
 }
+
+
+const nayoks=[ 'Bangla Bhai', 'English Bhai', 'Korean Bhai' ,'Turkish Bhai' ]
+
+for(let i = 0; i <= 5; i++){
+    console.log(nayoks)
+}
+
+let sum=0; 
+for( let i = 0; i<=3;i++){ 
+sum = sum + i; 
+}
+console.log(sum)
