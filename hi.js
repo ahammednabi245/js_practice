3883b14de78494b3587dbff4ef03d96e46fda3e3
ahammednabi245 <kaziahammed245nabi@gmail.com>
2023-02-24 11:03:48 +0600

    new practice

---
+++ hi.js
@@ -0,0 +1,9 @@
+// function getSum(a, b=9){
+//     return a+b;
+// } 
+// console.log(getSum(2, 7));
+
+const y = x => x*x; 
+const z = y(5);
+
+console.log(z);
---
+++ practice_problem.js
@@ -0,0 +1,55 @@
+// const multiply = (a, b, c) => a*b*c;
+// const total = multiply(2, 2, 2);
+// console.log(total);
+
+
+// function multiply(a,b,c){
+//     return a*b*c;
+// }
+
+// const total = multiply(2, 2, 2);
+// console.log(total);
+
+
+
+// const sentence = " I am a web developer.\n I love to code.\n I love to eat biryani.";
+// console.log(sentence);
+
+
+
+// function min(nums){ 
+//     return Math.min(nums) 
+//   }
+//   console.log(min( [1,3,2 ]))
+
+
+// const cube=x=> x*x*x; 
+// console.log(cube(2))
+
+// const [a, b] = [1,2,3,4,45,5]; 
+// console.log(a+b);
+
+
+
+// const {x, y, z} = {x: 1, y1: 2, z: 3};
+
+// console.log(x, y, z)
+
+
+
+
+// const nums = [1,2,3,4,5];
+// let output = nums.filter(n => n%2);
+// console.log(output);
+
+
+
+// const friends = ["Moushumi", "Misha", "Manna", "mimi" , "mahiya"];
+
+// const friendWithFiveChars = friends.find(friend => friend.length === 5);
+
+// console.log(friendWithFiveChars); // "Misha"
+
+
+const stuInfo = JSON.stringify({ name: "James", roll: 3 }); 
+console.log(stuInfo.name);
