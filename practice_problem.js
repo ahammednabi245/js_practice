5fa80d59d3129e5f514f0b9c740b5728d8be6ea3
ahammednabi245 <kaziahammed245nabi@gmail.com>
2023-02-28 10:43:00 +0600

    Inside

--- practice_problem.js
+++ practice_problem.js
@@ -55,5 +55,45 @@
 // console.log(stuInfo.name);
 
 
-const obj ={a:1, b:7, c:3, length:2};
-console.log(Object.keys(obj).length);
+// const obj ={a:1, b:7, c:3, length:2};
+// console.log(Object.keys(obj).length);
+
+
+// const obj1= {module: 35, video:2}; 
+// const obj2= {module: 35, video:2};
+// console.log(obj1 === obj2);
+
+
+// const getGirlFriend= (name = "chokina")=>"name"; console.log(getGirlFriend()); 
+
+// let p='Javascript'; 
+// let q=p; 
+// p='React'; 
+// console.log(q); 
+
+
+// const isTrue='false'; 
+// if(!isTrue){
+// console.log('hello');
+// } else {
+// console.log('world'); 
+// }
+
+
+// function sum(p, q) {
+//     p + q;
+//    }
+//    const result = sum(2, 3);
+//    console.log(result);
+
+
+// if ("2" === 2) {
+//     console.log("Inside if");
+//    } else {
+//     console.log("Inside else");
+//    }
+
+// function work(x, y = 4) {
+//     return x + y;
+//    }
+//    console.log(work(32);
